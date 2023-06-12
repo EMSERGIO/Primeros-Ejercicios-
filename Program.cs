290add61d12dbb577a3575a65ca91edbfa66de1b
Laura Mariscal <sergiomeriogrande@gmail.com>
2023-06-12 11:35:15 -0300

    Primera Subida

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicios
+
+            //1. Hacer un programa para solicitar dos números y luego calcule y emita la suma en pantalla.
+            //int num1, num2, resultado;
+
+            // Console.WriteLine("Dame el primer numero");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Dame el segundo numero");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //resultado = num1 * num2;
+
+            //Console.WriteLine("El resultado de la multiplicacion es:" + resultado);
+
+
+            //.......................................................................
+
+            //2. Hacer un programa para solicitar por teclado un número 
+            //...y luego devolver su valor elevado al cubo.
+
+            // int num1, multiplicacion;
+
+            // Console.WriteLine("Dme el numero");
+            // num1 = int.Parse(Console.ReadLine());
+
+            // multiplicacion = num1 * num1 * num1;
+
+            // Console.WriteLine("El resultado del numero elevado al cubo es:" + multiplicacion);
+
+            // .......................................................................
+
+            // 3. Hacer un programa que permita ingresar los kilómetros existentes entre dos ciudades y la velocidad 
+            // promedio de un vehículo. Calcular y emitir por pantalla el tiempo aproximado que demandará 
+            // llegar de un punto a otro teniendo en cuenta los datos ingresados.
+            // float km, velocidad, tiempo;
+
+            // Console.WriteLine("Ingrese los Km que hay entre ciudades");
+            // km = float.Parse(Console.ReadLine());
+
+            // Console.WriteLine("A que velocidad hace su viaje");
+            // velocidad = float.Parse(Console.ReadLine());
+
+            // tiempo = km / velocidad;
+
+            // Console.WriteLine("El tiempo que le llevara en viajar de una ciudad a otra es de:" + tiempo.ToString("0.00"));
+
+            //tiempo.ToString("0.00") ejemplo de como achicar el numero despues del punto
+
+            // .......................................................................
+
+
+            // 4. Una casa de computación paga a sus empleados un sueldo fijo de ARS15000 más una comisión
+            // del 5% sobre el total facturado por cada empleado. Hacer un programa para ingresar de
+            // total facturado por un empleado y que luego calcule y emita por pantalla el sueldo total a cobrar por el mismo.
+            // float totalFacturado, comisión,sueldoFinal;
+
+            // Console.WriteLine("Ingrese el total facturado del mes en curso");
+            // totalFacturado = float.Parse(Console.ReadLine());
+
+            // comisión = 5 * totalFacturado / 100;
+            // sueldoFinal = comisión + 15000;
+
+            // Console.WriteLine("El total a cobrar del mes en curso es:" + sueldoFinal);
+
+            // .......................................................................
+
+            // 5. Hacer un programa para ingresar por teclado las tres notas de exámenes de un alumno y
+            // luego calcule y emita por pantalla el promedio final.
+            // float n1, n2, n3, promedio;
+
+            // Console.WriteLine("Ingrese la primer nota");
+            // n1 = float.Parse(Console.ReadLine());
+            // Console.WriteLine("Ingrese la segunda nota:");
+            // n2 = float.Parse(Console.ReadLine());
+            // Console.WriteLine("Ingrese la tercera nota:");
+            // n3 = float.Parse(Console.ReadLine());
+
+            // promedio = (n1 + n2 + n3) / 3;
+            // Console.WriteLine("Su promedio final es de:" + promedio.ToString("0.00"));
+
+
+
+
+
+        }
+    }
+}
